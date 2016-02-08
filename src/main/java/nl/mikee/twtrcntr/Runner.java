c6f6f523061acc4545e3b1ff3c2915664fcd0193
mike <mailmelk@gmail.com>
2016-02-08 16:01:18 +0100

    tweaks

--- src/main/java/nl/mikee/twtrcntr/Runner.java
+++ src/main/java/nl/mikee/twtrcntr/Runner.java
@@ -1,5 +1,7 @@
 package nl.mikee.twtrcntr;
 
+import java.time.LocalDate;
+
 /**
  * Created by mike on 06/02/16.
  */
@@ -10,7 +12,6 @@ public class Runner {
     SearchCounter searchCounter;
     SearchCounterDAO searchCounterDAO = new SearchCounterDAO();
 
-
     public static void main(String[] args) {
         Runner runner = new Runner();
         runner.go();
@@ -19,17 +20,18 @@ public class Runner {
     private void go() {
         Tweets tweets = new Tweets(config.getTwitterInstance());
 
-
         if(searchHandler.getSearches() > 0) {
 
+            for(SearchEntity se: searchHandler.getAllSearchers()) {
+                String hashtag = se.getHashTag();
+                LocalDate since = LocalDate.now().minusDays(se.getDaysBack());
+                LocalDate until = since.plusDays(se.getPeriodInDays());
 
-        for(SearchEntity se: searchHandler.getAllSearchers()) {
-            searchCounter = tweets.countHashTag(se.getHashTag());
-            searchCounterDAO.saveSearchCounter(searchCounter);
-            searchCounter.toString();
+                searchCounter = tweets.countHashTag(hashtag, since, until);
 
-        }
+                searchCounterDAO.saveSearchCounter(searchCounter);
 
+            }
 
         } else {
             System.out.println("No searches found.");
--- src/main/java/nl/mikee/twtrcntr/Tweets.java
+++ src/main/java/nl/mikee/twtrcntr/Tweets.java
@@ -1,11 +1,10 @@
 package nl.mikee.twtrcntr;
 
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
+import twitter4j.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * Created by mike on 06/02/16.
@@ -18,15 +17,44 @@ public class Tweets {
        this.twitter = twitter;
     }
 
-    public SearchCounter countHashTag(String hashTag) {
+    public SearchCounter countHashTag(String hashTag, LocalDate since, LocalDate until) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String sinceAsString = since.format(formatter);
+        String untilAsString = until.format(formatter);
+
+        return countHashTag(hashTag, sinceAsString, untilAsString);
+
+    }
+
+    public SearchCounter countHashTag(String hashTag, String since, String until) {
         SearchCounter searchCounter = new SearchCounter();
-        Query query = new Query(hashTag);
+        Query query = new Query(hashTag).since(since).until(until).count(100);
+        int retweetCount = 0, tweetCount = 0;
+
         try {
             QueryResult result = twitter.search(query);
+
+
+            do {
+                result = twitter.search(query);
+
+                tweetCount += result.getTweets().size();
+
+                for (Status tweetStatus : result.getTweets()) {
+
+                    retweetCount += tweetStatus.getRetweetCount();
+                    query.setMaxId(tweetStatus.getId());
+
+                }
+
+            } while (result.hasNext());
+
+
             searchCounter.setHashTag(hashTag);
-            searchCounter.setCounter(result.getTweets().size());
-            searchCounter.setDate(LocalDate.now().minusDays(1L));
-            searchCounter.setRetweets(result.getCount());
+            searchCounter.setCounter(tweetCount);
+            searchCounter.setDate(LocalDate.parse(since));
+            searchCounter.setRetweets(retweetCount);
 
             return searchCounter;
         }catch(TwitterException e) {
